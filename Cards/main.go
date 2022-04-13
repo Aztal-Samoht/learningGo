cc09fbd334830fcc03ed0d7e8f8aa5f5b8ea4b79
Aztal <thatguysquig@gmail.com>
2022-04-13 11:45:55 -0500

    added code for for loop to cards project

--- Cards/main.go
+++ Cards/main.go
@@ -5,7 +5,16 @@ import "fmt"
 func main() {
 	card := newCard()
 	fmt.Println(card)
+
+	cards := []string{newCard(), newCard(), "ace of spades"	}
+	fmt.Println(cards)
+	cards = append(cards, "six of spades")
+	for i, aCard := range cards{
+		fmt.Println(i)
+		fmt.Println(aCard)
+	}
 }
+	
 
 func newCard() string {
 	return "five of diamonds"
