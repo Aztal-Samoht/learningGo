8a37fe5f978b27c6114d6f02e0d3462ccb08e2fe
Aztal <thatguysquig@gmail.com>
2022-04-13 14:09:37 -0500

    moved the print func to deck.go

--- Cards/deck.go
+++ Cards/deck.go
@@ -1,1 +1,18 @@
 package main
+
+import "fmt"
+
+//create a new type of deck which is a slice of stings
+type deck []string
+
+func (d deck) print(){
+	for i, aCard := range d{
+		fmt.Println(i, aCard)
+	}
+}
+
+func (d deck) addQ() deck{
+	d = append(d, "queen of hearts")
+	return d
+	//d.print()
+}
--- Cards/main.go
+++ Cards/main.go
@@ -6,13 +6,11 @@ func main() {
 	card := newCard()
 	fmt.Println(card)
 
-	cards := []string{newCard(), newCard(), "ace of spades"	}
+	cards := deck{newCard(), newCard(), "ace of spades"	}
 	fmt.Println(cards)
 	cards = append(cards, "six of spades")
-	for i, aCard := range cards{
-		fmt.Println(i)
-		fmt.Println(aCard)
-	}
+	cards.addQ()
+	cards.print()
 }
 	
 
